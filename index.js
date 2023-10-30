118639c5ac04003b94bc271df51280dd8e2c7c03
adityaekasantoso12 <adityaekasantoso12@gmail.com>
2023-10-30 19:19:09 +0700

    add code video 3

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const express = require('express')
 const app = express()
+const userrouter = require('./router/users')
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/users', (req, res) => {
-    res.send('get all users')
-  })
-app.post('/user', (req, res) => {
-    res.send('Get a POST request')
-  })
-
-app.put('/user/:id', (req, res) => {
-    res.send('Got a PUT request at /user')
-  })
-app.delete('/user/:id', (req, res) => {
-    res.send('Got a DELETE request at /user')
-  })
+app.use(userrouter)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
